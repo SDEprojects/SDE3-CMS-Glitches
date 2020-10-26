ffda679d6b93f38b41967e077cdd0de66fdd1650
Chris Calso <christopher.calso@gmail.com>
2020-10-26 14:10:59 -0700

    Brickbreaker game

---
+++ src/com/brickbreaker/StartBrickBreaker.java
@@ -0,0 +1,17 @@
+package com.brickbreaker;
+import javax.swing.JFrame;
+import com.glitches.models.Player;
+
+public class StartBrickBreaker {
+    public static void main() {
+        JFrame obj = new JFrame();
+        BrickPanel gamePlay = new BrickPanel();
+
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
